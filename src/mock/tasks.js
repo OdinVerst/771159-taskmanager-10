afeca1a1e70a2d43a5eaf02380c44360561c4b13
Sergey Razdolyansy <grafcorp01@gmail.com>
2019-11-29 10:36:49 +0300

    Добавление цветов в task

--- src/mock/tasks.js
+++ src/mock/tasks.js
@@ -1,9 +1,16 @@
 import {getRandomItem} from "../utils";
 
 const DescriptionItems = [`Изучить теорию`, `Сделать домашку`, `Пройти интенсив на соточку`];
+const ColorItems = [`black`, `yellow`, `blue`, `green`, `pink`];
 
 export const generateTask = () => {
   return {
     description: getRandomItem(DescriptionItems),
+    dueDate: 1,
+    repeatingDays: 1,
+    tags: true,
+    color: getRandomItem(ColorItems),
+    isFavorite: true,
+    isArchive: false
   };
 };
