4ac856c7f03c33d2191b625f1461009c79c6f05c
Sergey Razdolyansy <grafcorp01@gmail.com>
2019-11-30 22:09:36 +0300

    Логика загрузить еще

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
-import {templateFilter} from './components/filter';
-import {templateBoard} from './components/board';
-import {templateTask} from './components/task';
-import {templateBtnMore} from './components/btn-more';
-import {templateMenu} from './components/menu';
-import {templateEditTask} from './components/task-edit';
+import {templateFilter} from "./components/filter";
+import {templateBoard} from "./components/board";
+import {templateTask} from "./components/task";
+import {templateBtnMore} from "./components/btn-more";
+import {templateMenu} from "./components/menu";
+import {templateEditTask} from "./components/task-edit";
 
-import {generateTask} from './mock/tasks';
-import {generateFilters} from './mock/filter';
-
+import {generateTask} from "./mock/tasks";
+import {generateFilters} from "./mock/filter";
 
 const render = (container, template) => {
   container.insertAdjacentHTML(`beforeend`, template);
@@ -27,12 +26,33 @@ const boardTask = document.querySelector(`.board__tasks`);
 render(boardTask, templateEditTask(generateTask()));
 
 const COUNT_TASKS = 22;
+const SHOWING_TASKS_COUNT_ON_START = 8;
+const SHOWING_TASKS_COUNT_BY_BUTTON = 8;
 
-[...new Array(COUNT_TASKS)].forEach(() => {
-  render(boardTask, templateTask(generateTask()));
-});
+let tasksOnBoard = 1;
+
+const createTasks = (count) => {
+  [...new Array(count)].forEach(() => {
+    render(boardTask, templateTask(generateTask()));
+  });
+  tasksOnBoard += count;
+};
+
+createTasks(SHOWING_TASKS_COUNT_ON_START - 1);
 
 const board = document.querySelector(`.board`);
 
 render(board, templateBtnMore());
 
+const loadMoreButton = main.querySelector(`.load-more`);
+loadMoreButton.addEventListener(`click`, () => {
+  let balanseTasks = COUNT_TASKS - tasksOnBoard;
+  if (balanseTasks) {
+    if (balanseTasks - SHOWING_TASKS_COUNT_BY_BUTTON >= 1) {
+      createTasks(SHOWING_TASKS_COUNT_BY_BUTTON);
+    } else {
+      createTasks(balanseTasks);
+      loadMoreButton.remove();
+    }
+  }
+});
