eea9fc803587d0db9c39dc559e8d5c60fcb7447e
Sergey Razdolyansy <grafcorp01@gmail.com>
2019-12-03 18:05:46 +0300

    Замена карточки задачи на форму редактирования

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import Menu from "./components/menu";
 import Filter from "./components/filter";
 import Board from "./components/board";
 import Task from "./components/task";
+import TaskEdit from "./components/task-edit";
 import BtnMore from "./components/btn-more";
-import {render} from "./utils";
+import {render, RenderPosition} from "./utils";
 import {generateTasks} from "./mock/tasks";
 import {generateFilters} from "./mock/filter";
 
@@ -15,39 +16,56 @@ let tasksOnBoard = 0;
 const mainControlElement = document.querySelector(`.main__control`);
 const mainElement = document.querySelector(`.main`);
 
-render(mainControlElement, new Menu().getElement(), `beforeend`);
+render(mainControlElement, new Menu().getElement(), RenderPosition.BEFOREEND);
 
 const filters = generateFilters(ALL_TASKS);
-render(mainElement, new Filter(filters).getElement(), `beforeend`);
+render(mainElement, new Filter(filters).getElement(), RenderPosition.BEFOREEND);
 
-render(mainElement, new Board().getElement(), `beforeend`);
+render(mainElement, new Board().getElement(), RenderPosition.BEFOREEND);
 
 const boardTask = document.querySelector(`.board__tasks`);
 
-const createTasks = (count) => {
+const createTask = (task) => {
+  const taskComponent = new Task(task);
+  const taskEditComponent = new TaskEdit(task);
+
+  const editBtn = taskComponent.getElement().querySelector(`.card__btn--edit`);
+  editBtn.addEventListener(`click`, () => {
+    boardTask.replaceChild(taskEditComponent.getElement(), taskComponent.getElement());
+  });
+
+  const submitEditTask = taskEditComponent.getElement().querySelector(`.card__form`);
+  submitEditTask.addEventListener(`click`, () => {
+    boardTask.replaceChild(taskComponent.getElement(), taskEditComponent.getElement());
+  });
+
+  render(boardTask, taskComponent.getElement(), RenderPosition.BEFOREEND);
+};
+
+const renderTasks = (count) => {
   const itaration = Math.round(tasksOnBoard / SHOWING_TASKS_COUNT_ON_ITERATION);
   const start = (itaration * SHOWING_TASKS_COUNT_ON_ITERATION);
   const end = start + count;
   ALL_TASKS.slice(start, end).forEach((task) => {
-    render(boardTask, new Task(task).getElement(), `beforeend`);
+    createTask(task);
   });
   tasksOnBoard += count;
 };
 
-createTasks(SHOWING_TASKS_COUNT_ON_ITERATION);
+renderTasks(SHOWING_TASKS_COUNT_ON_ITERATION);
 
 const board = document.querySelector(`.board`);
 
 const btnMore = new BtnMore();
-render(board, btnMore.getElement(), `beforeend`);
+render(board, btnMore.getElement(), RenderPosition.BEFOREEND);
 
 btnMore.getElement().addEventListener(`click`, () => {
   let balanseTasks = COUNT_TASKS - tasksOnBoard;
   if (balanseTasks) {
     if (balanseTasks - SHOWING_TASKS_COUNT_ON_ITERATION >= 1) {
-      createTasks(SHOWING_TASKS_COUNT_ON_ITERATION);
+      renderTasks(SHOWING_TASKS_COUNT_ON_ITERATION);
     } else {
-      createTasks(balanseTasks);
+      renderTasks(balanseTasks);
       btnMore.getElement().remove();
       btnMore.removeElement();
     }
