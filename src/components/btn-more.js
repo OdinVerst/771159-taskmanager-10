66dd0021ef1578cc79fd96178214cb2e73d0348c
Sergey Razdolyansy <grafcorp01@gmail.com>
2019-12-03 10:30:45 +0300

    Кнопка показать еще переписана на классы

--- src/components/btn-more.js
+++ src/components/btn-more.js
@@ -1,3 +1,27 @@
-export const templateBtnMore = () => {
+import {createElement} from '../utils';
+
+const createBtnMoretemplate = () => {
   return `<button class="load-more" type="button">load more</button>`;
 };
+
+export default class BtnMore {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createBtnMoretemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
