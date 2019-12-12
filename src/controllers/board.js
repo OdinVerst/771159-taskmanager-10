cdaa94816be4f1f6036795ce859bd0910f69f7e1
Sergey Razdolyansy <grafcorp01@gmail.com>
2019-12-12 10:19:12 +0300

    Move logic render Task to taskcontroller

--- src/controllers/board.js
+++ src/controllers/board.js
@@ -1,50 +1,20 @@
-import Task from "../components/task";
-import TaskEdit from "../components/task-edit";
-import {replace, render, remove} from "../utils/render";
+import {render, remove} from "../utils/render";
 import NoTasks from "../components/no-tasks";
 import BoardSort, {SortType} from "../components/board-sort";
 import BtnMore from "../components/btn-more";
 import BoardTaskList from "../components/board-tasks-list";
+import TaskController from "./task";
 
 let tasksOnBoard = 0;
 const SHOWING_TASKS_COUNT_ON_ITERATION = 8;
-
-const createTask = (container, task) => {
-  const taskComponent = new Task(task);
-  const taskEditComponent = new TaskEdit(task);
-
-  const onEscKeyDown = (evt) => {
-    const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
-
-    if (isEscKey) {
-      replaceEditToTask();
-      document.removeEventListener(`keydown`, onEscKeyDown);
-    }
-  };
-
-  const replaceEditToTask = () => {
-    replace(taskComponent, taskEditComponent);
-  };
-
-  const replaceTaskToEdit = () => {
-    replace(taskEditComponent, taskComponent);
-  };
-
-  taskComponent.setEditButtonClickHandler(() => {
-    replaceTaskToEdit();
-    document.addEventListener(`keydown`, onEscKeyDown);
-  });
-
-  taskEditComponent.setSubmitHandler(replaceEditToTask);
-  render(container, taskComponent);
-};
 
 const renderTasks = (container, tasks, count = tasks.length) => {
   const itaration = Math.round(tasksOnBoard / SHOWING_TASKS_COUNT_ON_ITERATION);
   const start = itaration * SHOWING_TASKS_COUNT_ON_ITERATION;
   const end = start + count;
   tasks.slice(start, end).forEach((task) => {
-    createTask(container, task);
+    const taskControler = new TaskController(container);
+    taskControler.render(task);
   });
   tasksOnBoard += count;
 };
--- src/controllers/task.js
+++ src/controllers/task.js
@@ -1,13 +1,45 @@
 import Task from "../components/task";
 import TaskEdit from "../components/task-edit";
+import {render, replace} from "../utils/render";
 
 export default class TaskController {
   constructor(container) {
     this._container = container;
+    this._taskComponent = [];
+    this._taskEditComponent = [];
+
+    this._onEscKeyDown = this._onEscKeyDown.bind(this);
   }
 
   render(task) {
     this._taskComponent = new Task(task);
     this._taskEditComponent = new TaskEdit(task);
+
+    this._taskEditComponent.setSubmitHandler(() => {
+      this._replaceEditToTask();
+    });
+    this._taskComponent.setEditButtonClickHandler(() => {
+      this._replaceTaskToEdit();
+      document.addEventListener(`keydown`, this._onEscKeyDown);
+    });
+
+    render(this._container, this._taskComponent);
+  }
+
+  _replaceEditToTask() {
+    replace(this._taskComponent, this._taskEditComponent);
+  }
+
+  _replaceTaskToEdit() {
+    replace(this._taskEditComponent, this._taskComponent);
+  }
+
+  _onEscKeyDown(evt) {
+    const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
+
+    if (isEscKey) {
+      this._replaceEditToTask();
+      document.removeEventListener(`keydown`, this._onEscKeyDown);
+    }
   }
 }
